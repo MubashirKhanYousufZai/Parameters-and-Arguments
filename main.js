7633ad11f4784514c0b6fa62e791f2a7e3151cb8
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-04-10 13:37:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Parameters and Arguments
+function greek(num, num2) {
+    return num + num2;
+}
+let result = greek(24, 6);
+console.log(result);
+function totalMarks(paper, paper2) {
+    return paper + paper2;
+}
+let exam = totalMarks(24, 50);
+console.log(exam);
---
+++ main.ts
@@ -0,0 +1,12 @@
+// Parameters and Arguments
+function greek(num: number, num2: number): number {
+    return num + num2;
+}
+let result: number = greek(24,6)
+console.log(result);
+
+function totalMarks(paper: number , paper2: number): number {
+    return paper + paper2;
+}
+let exam: number = totalMarks(24 , 50)
+console.log(exam);
